62e3e00dfbdd59edf66fba656f66368c0de9a946
kavindugeethshan <k.geethshan@gmail.com>
2026-07-20 20:42:52 +0530

    Import express , run express , create arrow functions , create GET POST PUT DELEAT request and check on postman , install nodemon

--- G - Lab - V 1/server.js
+++ G - Lab - V 1/server.js
@@ -1,1 +1,23 @@
-console.log("Server is running");
+import express from 'express';
+let app =express(); 
+
+app.get("/",()=>{
+    console.log("Get request received")
+});
+
+app.post("/",()=>{
+    console.log("Post request received")
+});
+
+app.put("/",()=>{
+    console.log("Put request received")
+});
+
+app.delete("/",()=>{
+    console.log("Delete request received")
+});
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
