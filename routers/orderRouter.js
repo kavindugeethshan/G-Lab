b24acd9e9682106fe60a7952a478ee41cdc036c2
kavindugeethshan <k.geethshan@gmail.com>
2026-08-20 19:44:36 +0530

    Fix middleware import path

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../Middleware/authMiddleware.js";
 import { createOrder, getMyOrders, getOrderById } from "../Controllers/OrderController.js";
 
 const orderRouter = express.Router();
